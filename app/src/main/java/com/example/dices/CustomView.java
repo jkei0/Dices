feb94bafb5e49d3f02f11f8e0ddd9a734e0d037c
Joni Keinänen <joni.keinanen@student.tut.fi>
2020-02-10 04:21:47 +0200

    Updated graphics

---
+++ app/src/main/java/com/example/dices/CustomView.java
@@ -0,0 +1,39 @@
+package com.example.dices;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View {
+    private final int paintColor = Color.RED;
+    private Paint drawPaint;
+
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawPaint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
+
+}
--- app/src/main/java/com/example/dices/MainActivity.java
+++ app/src/main/java/com/example/dices/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 
@@ -18,13 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
-    public void pressButton(View view) {
+    public void pressButton(View view)
+    {
 
-        // Values of each dice
-        String d1 = "ARMSTYPUKSCL";
-        String d2 = "*JRHTDTOVPSV";
-        String d3 = "*LNBKÖ*PEKIT";
-        String d4 = "GOMAFEHUSEJÄ";
+        // Value for each dice
+        List<Integer> d1 = Arrays.asList(R.drawable.a,R.drawable.r,R.drawable.m,R.drawable.s,R.drawable.t,R.drawable.y,
+                R.drawable.p,R.drawable.u,R.drawable.k,R.drawable.s,R.drawable.c,R.drawable.l);
+        //String d1 = "ARMSTYPUKSCL";
+
+        List<Integer> d2 = Arrays.asList(R.drawable.tahti,R.drawable.j,R.drawable.r,R.drawable.h,R.drawable.t,
+                R.drawable.d,R.drawable.t,R.drawable.o,R.drawable.v,R.drawable.p,R.drawable.s,R.drawable.v);
+        //String d2 = "*JRHTDTOVPSV";
+
+        List<Integer> d3 = Arrays.asList(R.drawable.tahti,R.drawable.l,R.drawable.n,R.drawable.b,
+                R.drawable.k,R.drawable.oo,R.drawable.tahti,R.drawable.p,R.drawable.e,R.drawable.k,R.drawable.i,R.drawable.t);
+        //String d3 = "*LNBKÖ*PEKIT";
+
+        List<Integer> d4 = Arrays.asList(R.drawable.g,R.drawable.o,R.drawable.m,R.drawable.a,
+                                            R.drawable.f,R.drawable.e,R.drawable.h,R.drawable.u,R.drawable.s,
+                                            R.drawable.e,R.drawable.j,R.drawable.aa);
+        //String d4 = "GOMAFEHUSEJÄ";
 
         // Randomly select value for each dice
         Random rand = new Random();
@@ -34,15 +50,20 @@ public class MainActivity extends AppCompatActivity {
         int n4 = rand.nextInt(12);
 
 
-        TextView dice_1 = findViewById(R.id.dice1);
-        TextView dice_2 = findViewById(R.id.dice2);
-        TextView dice_3 = findViewById(R.id.dice3);
-        TextView dice_4 = findViewById(R.id.dice4);
+        ImageView dice_1 = findViewById(R.id.dice_1);
+        ImageView dice_2 = findViewById(R.id.dice_2);
+        ImageView dice_3 = findViewById(R.id.dice_3);
+        ImageView dice_4 = findViewById(R.id.dice_4);
 
-        dice_1.setText(String.valueOf(d1.charAt(n1)));
-        dice_2.setText(String.valueOf(d2.charAt(n2)));
-        dice_3.setText(String.valueOf(d3.charAt(n3)));
-        dice_4.setText(String.valueOf(d4.charAt(n4)));
+        Integer i1 = d1.get(n1);
+        Integer i2 = d2.get(n2);
+        Integer i3 = d3.get(n3);
+        Integer i4 = d4.get(n4);
+
+        dice_1.setImageResource(i1);
+        dice_2.setImageResource(i2);
+        dice_3.setImageResource(i3);
+        dice_4.setImageResource(i4);
 
     }
 
